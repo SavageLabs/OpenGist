387438eec22f34279b5352b989fe2640bae55489
Liz3 <accs@21xayah.com>
2019-10-06 06:10:23 +0200

    added stuff

--- database/DatabaseConnection.js
+++ database/DatabaseConnection.js
@@ -1,9 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 const api = require('./DatabaseApi');
+const isProd = process.env.NODE_ENV === "production";
+
 
 
 exports.startDatabase = (host, port, databaseName, callback) => {
-    const client = new MongoClient(`mongodb://${host}:${port}`);
+    let client;
+  if (!isProd) {
+    client = new MongoClient(`mongodb://${host}:${port}`);
+  } else {
+    client = new MongoClient(
+      `mongodb://${process.env.db_user}:${process.env.db_password}@${process.env.db_host}:${port}/?authMechanism=DEFAULT&authSource=${databaseName}`
+    );
+  }
     client.connect(function(err) {
         if(err) {
             callback(err, null);
